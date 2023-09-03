b11941f7f7c1e353af52eb5e00b54d302ba19937
sayed1000000 <szsz20177@yahoo.com>
2023-09-03 20:53:35 +0300

    first virsion

---
+++ store/database/migrations/2023_08_15_102241_create_orders_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id')->unsigned();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->integer('product_color_size_id')->unsigned();
+
+            $table->foreign('product_color_size_id')->references('id')->on('product_color_size');
+            $table->integer('quantity');
+            $table->decimal('price',10,2)->nullable();
+            $table->decimal('discount',10,2)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders_details');
+    }
+};
